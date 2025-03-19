65352aeded2c2d3dbddd0604f997f76bcfda9728
Aris Giotis <apjoti@gmail.com>
2025-03-19 15:17:12 +0200

    feat: connect redux with react spa

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { AppDispatch } from "./redux/store";
+import { fetchCharacters } from "./redux/thunk";
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <>
-      <Button>Click Me</Button>
+      <Button
+        onClick={() => {
+          dispatch(fetchCharacters());
+        }}
+      >
+        Play Now
+      </Button>
     </>
   );
 }
